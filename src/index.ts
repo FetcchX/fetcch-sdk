d60ff9c9d4ba24ce78a4713afa1e11f1b4d9fa47
Satyam Kulkarni <punekar.satyam@gmail.com>
2023-10-23 14:50:10 +0530

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import request from "./request"
+import addressBook from "./address-book"
+import identity from "./identity"
+import utils from "./utils"
+
+export default {
+	request,
+	addressBook,
+	identity,
+	utils
+}
