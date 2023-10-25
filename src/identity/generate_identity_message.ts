1a87ed6a7649617d436bfa33dd483b27f1bf2311
Rahul Kulkarni <rahulkulkarni5234@gmail.com>
2023-10-25 15:12:19 +0530

    Feat: added test for generate identity message

--- src/identity/generate_identity_message.ts
+++ src/identity/generate_identity_message.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import Apis from "../utils/api_config";
 import {generateIdentityMessageSchema} from "../types/identity";
 
-export const generateIdentityMessage = async (data: generateIdentityMessageSchema): Promise<any[]> => {
+export const generateIdentityMessage = async (data: generateIdentityMessageSchema,secret:string): Promise<any[]> => {
     const req = await axios({
         url: Apis.wIdentityGenerateMessageEndpoint,
         method: "POST",
-       
         headers: {
+            "secret-key":secret
             // ...headers,
         },
         data
