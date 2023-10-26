382aa21e9baa8f75a7d016a25d4e1ebdf2b0f159
Rahul Kulkarni <rahulkulkarni5234@gmail.com>
2023-10-26 16:55:07 +0530

    Feat: Auth functions added

---
+++ src/auth/login_message.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import Apis from "../utils/api_config";
+import { getSecretKey } from "..";
+
+export const generateMessage = async (owner:string): Promise<any[]> => {
+    const data = {
+        "owner": owner
+    };
+    
+    const req = await axios({
+        url: Apis.wGenerateAuthenticationMessageEndpoint,
+        method: 'POST',
+        headers: {
+            'secret-key': getSecretKey()
+        },
+        data
+    })
+
+    const res = await req.data
+
+    return res.data
+}
